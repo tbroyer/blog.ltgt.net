ff76126254d18c7f916af69e0c45d7f484e111bc
Thomas Broyer <t.broyer@ltgt.net>
2020-07-05 19:53:57 +0200

    Fix serve.sh for recent docker images, also add livereload support

--- serve.sh
+++ serve.sh
@@ -1,1 +1,1 @@
-docker run --rm -v "$PWD":/srv/jekyll -p "4000:4000" -e DRAFTS=true jekyll/jekyll:pages
+docker run --rm -it -v "$PWD":/srv/jekyll -p "4000:4000" -p "35729:35729" -e DRAFTS=true jekyll/jekyll:pages jekyll serve "$@"
